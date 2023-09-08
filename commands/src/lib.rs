af684c8852a34c26161745f288d7b63bb61966b0
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2023-09-08 23:34:08 +0900

    add: EzKvd trait for sled::Db

--- commands/src/lib.rs
+++ commands/src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use serenity::framework::standard::macros::command;
+use serenity::framework::standard::CommandResult;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+pub(crate) mod kvs;
+
+#[command]
+pub async fn rg_webhook(_ctx: &Context, _msg: &Message) -> CommandResult {
+
+    Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
