68deb7835bb2e6c26722a0c16e8ace10495b38e2
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2023-09-11 11:02:10 +0900

    update: ContextからのDb取得を関数に切り出し

--- commands/src/lib.rs
+++ commands/src/lib.rs
@@ -1,12 +1,17 @@
 use std::sync::Arc;
 
+use anyhow::anyhow;
 use serenity::framework::standard::macros::command;
 use serenity::framework::standard::CommandResult;
 use serenity::model::channel::Message;
+use serenity::model::webhook::Webhook;
 use serenity::prelude::*;
+
 use sqlx::SqlitePool;
+use tracing::error;
 
 mod webhook;
+mod list;
 
 // Dbのラッパー
 struct UtDb;
@@ -34,10 +39,120 @@ struct MemberWebhook {
     webhook_url: String,
 }
 
+// ContextからDbを取得する
+async fn get_db(ctx: &Context) -> Option<Arc<SqlitePool>> {
+    let data_read = ctx.data.read().await;
+    let db = data_read
+        .get::<UtDb>();
 
-#[command]
-pub async fn rg_webhook(_ctx: &Context, _msg: &Message) -> CommandResult {
+    match db {
+        Some(db) => {
+            let db = db.clone();
+            Some(db)
+        }
+        None => {
+            error!("db is None");
+            None
+        }
+        
+    }
+}
+
+
+async fn master_webhook_insert(
+    connection: &SqlitePool,
+    server_webhook: MasterWebhook,
+) -> anyhow::Result<()> {
+    sqlx::query!(
+        r#"
+        INSERT INTO master_webhooks (server_name, webhook_url)
+        VALUES(?, ?);
+        "#,
+        server_webhook.server_name,
+        server_webhook.webhook_url
+    )
+    .execute(connection)
+    .await?;
+
     Ok(())
+}
+
+async fn master_webhook_select(
+    connection: &SqlitePool,
+    server_name: &str,
+) -> anyhow::Result<MasterWebhook> {
+    let row = sqlx::query!(
+        r#"
+        SELECT * FROM master_webhooks WHERE server_name = ?;
+        "#,
+        server_name
+    )
+    .fetch_one(connection)
+    .await?;
+
+    let master_webhook = MasterWebhook {
+        id: Some(row.id),
+        server_name: row.server_name,
+        webhook_url: row.webhook_url,
+    };
+
+    Ok(master_webhook)
+}
+
+// メンバーwebhookの登録
+async fn member_webhook_insert(
+    connection: &SqlitePool,
+    member_webhook: MemberWebhook,
+) -> anyhow::Result<()> {
+    sqlx::query!(
+        r#"
+        INSERT INTO member_webhooks (server_name, user_id, webhook_url)
+        VALUES(?, ?, ?);
+        "#,
+        member_webhook.server_name,
+        member_webhook.user_id,
+        member_webhook.webhook_url
+    )
+    .execute(connection)
+    .await?;
+
+    Ok(())
+}
+
+// メンバーwebhookの取得
+async fn member_webhook_select(
+    connection: &SqlitePool,
+    server_name: &str,
+    user_id: i64,
+) -> anyhow::Result<MemberWebhook> {
+    let row = sqlx::query!(
+        r#"
+        SELECT * FROM member_webhooks WHERE server_name = ? AND user_id = ?;
+        "#,
+        server_name,
+        user_id
+    )
+    .fetch_one(connection)
+    .await?;
+
+    let member_webhook = MemberWebhook {
+        id: Some(row.id),
+        server_name: row.server_name,
+        user_id: row.user_id,
+        webhook_url: row.webhook_url,
+    };
+
+    Ok(member_webhook)
+}
+
+
+async fn create_webhook_from_channel(
+    ctx: &Context,
+    msg: &Message,
+    name: &str,
+) -> anyhow::Result<Webhook> {
+    let webhook = msg.channel_id.create_webhook(ctx, name).await?;
+    Ok(webhook)
 }
 
 // #[cfg(test)]
--- commands/src/webhook.rs
+++ commands/src/webhook.rs
@@ -1,93 +1,8 @@
-use serenity::model::webhook::Webhook;
+
 use tracing::{info, error};
 
 use crate::*;
 
-async fn master_webhook_insert(
-    connection: &SqlitePool,
-    server_webhook: MasterWebhook,
-) -> anyhow::Result<()> {
-    sqlx::query!(
-        r#"
-        INSERT INTO master_webhooks (server_name, webhook_url)
-        VALUES(?, ?);
-        "#,
-        server_webhook.server_name,
-        server_webhook.webhook_url
-    )
-    .execute(connection)
-    .await?;
-
-    Ok(())
-}
-
-async fn master_webhook_select(
-    connection: &SqlitePool,
-    server_name: &str,
-) -> anyhow::Result<MasterWebhook> {
-    let row = sqlx::query!(
-        r#"
-        SELECT * FROM master_webhooks WHERE server_name = ?;
-        "#,
-        server_name
-    )
-    .fetch_one(connection)
-    .await?;
-
-    let master_webhook = MasterWebhook {
-        id: Some(row.id),
-        server_name: row.server_name,
-        webhook_url: row.webhook_url,
-    };
-
-    Ok(master_webhook)
-}
-
-// メンバーwebhookの登録
-async fn member_webhook_insert(
-    connection: &SqlitePool,
-    member_webhook: MemberWebhook,
-) -> anyhow::Result<()> {
-    sqlx::query!(
-        r#"
-        INSERT INTO member_webhooks (server_name, user_id, webhook_url)
-        VALUES(?, ?, ?);
-        "#,
-        member_webhook.server_name,
-        member_webhook.user_id,
-        member_webhook.webhook_url
-    )
-    .execute(connection)
-    .await?;
-
-    Ok(())
-}
-
-// メンバーwebhookの取得
-async fn member_webhook_select(
-    connection: &SqlitePool,
-    server_name: &str,
-    user_id: i64,
-) -> anyhow::Result<MemberWebhook> {
-    let row = sqlx::query!(
-        r#"
-        SELECT * FROM member_webhooks WHERE server_name = ? AND user_id = ?;
-        "#,
-        server_name,
-        user_id
-    )
-    .fetch_one(connection)
-    .await?;
-
-    let member_webhook = MemberWebhook {
-        id: Some(row.id),
-        server_name: row.server_name,
-        user_id: row.user_id,
-        webhook_url: row.webhook_url,
-    };
-
-    Ok(member_webhook)
-}
 
 #[allow(non_snake_case)]
 #[command]
@@ -102,9 +17,7 @@ async fn UTregserver(ctx: &Context, msg: &Message) -> CommandResult {
     info!("server_name: {}, webhook_url: {}", server_name, webhook_url);
 
     // DBに登録する
-    let data_read = ctx.data.read().await;
-    let db = data_read
-        .get::<UtDb>();
+    let db = get_db(ctx).await;
 
     match db {
         Some(db) => {
@@ -136,11 +49,7 @@ async fn getMasterHook(ctx: &Context, msg: &Message) -> CommandResult {
     info!("server_name: {}", server_name);
 
     // DBから取得する
-    let data_read = ctx.data.read().await;
-    let db = data_read
-        .get::<UtDb>()
-        .expect("Expect UtDb in typemap")
-        .clone();
+    let db = get_db(ctx).await.ok_or(anyhow!("db is None"))?;
 
     let master_webhook = master_webhook_select(db.as_ref(), server_name).await?;
 
@@ -148,13 +57,4 @@ async fn getMasterHook(ctx: &Context, msg: &Message) -> CommandResult {
         .await?;
 
     Ok(())
-}
-
-async fn create_webhook_from_channel(
-    ctx: &Context,
-    msg: &Message,
-    name: &str,
-) -> anyhow::Result<Webhook> {
-    let webhook = msg.channel_id.create_webhook(ctx, name).await?;
-    Ok(webhook)
 }
