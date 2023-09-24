e81cb2114291263548a8f6f820482cbfa7c05e04
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2023-09-24 09:16:37 +0900

    sb

--- sb/crypt/src/main.rs
+++ sb/crypt/src/main.rs
@@ -1,49 +1,76 @@
-use rsa::{RsaPrivateKey, RsaPublicKey};
-use rsa::pkcs1v15::{SigningKey, VerifyingKey};
-use rsa::signature::{Keypair, RandomizedSigner, SignatureEncoding, Verifier};
+// use ring::{hmac, rand};
+// use ring::rand::SecureRandom;
+// use ring::error::Unspecified;
+
+// use crypto::common::Key;
+
+// use signature::{Signer, Verifier};
+use rsa::RsaPrivateKey;
+use rsa::pss::{BlindedSigningKey, SigningKey, VerifyingKey, Signature};
+use rsa::signature::{Keypair,RandomizedSigner, SignatureEncoding, Verifier};
 use rsa::sha2::{Digest, Sha256};
-// use rsa::pkcs8::{DecodePrivateKey, EncodePrivateKey, DecodePublicKey, EncodePublicKey};
 
-
-use spki::EncodePublicKey;
-use spki::DecodePublicKey;
-use base64ct::LineEnding;
+// use rsa::signature::rand_core::CryptoRngCore;
 
 fn main() {
-    println!("Hello, world!");
     let mut rng = rand::thread_rng();
-
     let bits = 2048;
-    let private_key = RsaPrivateKey::new(&mut rng, bits).unwrap();
-    let public_key = private_key.to_public_key();
-    public_key.to_public_key_pem(LineEnding::LF).unwrap();
 
-    // RsaPublicKey::from_public_key_pem(s)
+    let priv_key = rsa::RsaPrivateKey::new(&mut rng, bits).unwrap();
+    let pub_key = rsa::RsaPublicKey::from(&priv_key);
+    let sign_key: SigningKey<Sha256> = SigningKey::new(priv_key);
+    let veri_key: VerifyingKey<Sha256> = VerifyingKey::new(pub_key);
+    // let pub_key = rsa::RsaPublicKey::from(&priv_key);
 
-    // let a = private_key.to_pkcs8_pem(LineEnding::LF).unwrap().as_str();
+    let msg = "動け...サジェスト...! まだだ...! これからもっと 面白く...";
 
-    let signing_key = SigningKey::<Sha256>::new(private_key);
-    println!("signing_key: {:?}", signing_key);
+    // なんかサジェストが機能しない なんで？？
+    let signature: Signature = sign_key.sign_with_rng(&mut rng, "msg".as_bytes());
 
-    let verifying_key = signing_key.verifying_key();
-    println!("verifyng_key:{:?}", verifying_key);
+    println!("s: {:?}", &signature);
 
-    // //sign
-    // let data = b"Hello, world!";
-    // let signature = signing_key.sign_with_rng(&mut rng, data);
-    // println!("signature: {}", signature.to_string()); 
+    veri_key.verify(msg.as_bytes(), &signature).expect("faild to verify");
 
-    // //verify
-    // let a = verifying_key.verify(data, &signature).expect("failed to verify");
-    // println!("verify: {:?}", a);
+    // let sign_key:BlindedSigningKey<Sha256> = BlindedSigningKey::new(priv_key);
+
+    // let verifying_key = sign_key.verifying_key();
 
 
-    // let binding = signing_key.to_pkcs8_pem(LineEnding::LF).unwrap();
-    // let a = binding.as_str();
-    // println!("a: {:?}", &a);
+    
+    
+    // Verifier::verify(veri_key, "s", "");
+    // let message = b"hello world";
 
-    // let binding = signing_key.to_pkcs8_der();
+    // let a = priv_key.sign(Pss::new::<Sha256>(), message).unwrap();
+    // println!("{:?}",&a);
 
-    // let a:i32 = signing_key;
-    // let a: i32 = verifying_key;
+    // let b = pub_key.verify(Pss::new::<Sha256>(), message, &a).unwrap();
+
+
+
+
+
+
+
+
+    
+
+    // let veri_key = sign_key.verifying_key();
+
+
+
+    // priv_key.sign(padding, digest_in)
+
+
+    // let mut key_value = [0u8; 48];
+    // let rng = rand::SystemRandom::new();
+    // rng.fill(&mut key_value)?;
+    // let key = hmac::Key::new(hmac::HMAC_SHA256, &key_value);
+
+    // let message = "Hello, world!";
+    // let signature = hmac::sign(&key, message.as_bytes());
+
+    // hmac::verify(&key, message.as_bytes(), signature.as_ref())?;
+
+    // Ok(())
 }
