7500fe7855af52d95400bba153e9656d3cb4a877
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2024-03-11 14:45:23 +0900

    update: domain signer verifier

--- domain/src/traits/signer_verifier.rs
+++ domain/src/traits/signer_verifier.rs
@@ -1,20 +1,14 @@
 use crate::models::sign::Claims;
 
-pub trait UbiquitimesSigner {
+pub trait UtSigner {
     type Error;
     // 引数として渡すデータはまだ確定していない
     // 前に書いた自分のコードを読まなければ...
     fn sign(&self, claims: Claims) -> Result<String, Self::Error>;
 }
 
-pub trait UbiquitimesVerifier {
+pub trait UtVerifier {
     type Error;
 
     fn verify(&self, signed_token: &str) -> Result<Claims, Self::Error>;
-}
-
-trait TesTrait {
-    type Result<T>;
-
-    fn tes(&self) -> Self::Result<String>;
 }
